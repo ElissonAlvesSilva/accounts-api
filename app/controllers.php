d8a89ec86b54f667fb8a0420501294e1ae8b89ac
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2019-09-08 11:00:03 -0400

    feat: add new controllers call

--- app/controllers.php
+++ app/controllers.php
@@ -2,3 +2,11 @@
 $container['UsersController'] = function ($container) {
   return App\Controllers\UsersController($container);
 };
+
+$container['ApplicationsController'] = function ($container) {
+  return App\Controllers\ApplicationsController($container);
+};
+
+$container['PermissionsController'] = function ($container) {
+  return App\Controllers\PermissionsController($container);
+};
